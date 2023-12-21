e51f50c5277fa97b6ed45b785d99948b471dba31
xFooFoo <yarivfu@gmail.com>
2023-12-21 17:43:05 +1300

    Passed all FCC tests + bonus test

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import './App.css'
+import { marked } from 'marked';
+import defaultData from './assets/defaultData.tsx';
+
+// set options
+marked.use({
+    breaks: true
+})
+
+function App() {
+    const [editorContent, setEditorContent] = useState(defaultData);
+    const [previewContent, setPreviewContent] = useState(marked.parse(defaultData));
+
+    const handleEditorChange =  (event: { target: { value: string; }; }) => {
+        setEditorContent(event.target.value);
+        try {
+            const markedDownData =  marked.parse(event.target.value);
+            setPreviewContent(markedDownData);
+        }
+        catch (error) {
+            console.error('Error fetching data:', error);
+        }
+        
+    }
+
+
+    return (
+        <div className="background">
+            <div className="content">
+                <div className="toolbar">Editor</div>
+                <textarea id="editor" rows={15} cols={70} value={editorContent} onChange={handleEditorChange}></textarea>
+                <div className="toolbar">Preview</div>
+                <div id="preview" dangerouslySetInnerHTML={{ __html: previewContent }} ></div>
+            </div>
+        </div>
+    )
+}
+
+export default App
